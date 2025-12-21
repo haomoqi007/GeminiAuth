39b69c56a7635931e01ec404bd1896d9177ddedd
haomoqi007 <93587049+haomoqi007@users.noreply.github.com>
2025-12-21 22:42:39 +0800

    更新文档地址（在vercel地址添加）

--- api/config.js
+++ api/config.js
@@ -1,9 +1,11 @@
-// api/config.js
 export default function handler(req, res) {
-  // 从环境变量中读取，如果没有设置，则使用一个默认值（可选）
+  // 读取环境变量
   const logoUrl = process.env.LOGO_URL || '';
+  // 如果没设置文档地址，默认跳转到 # (即不跳转)
+  const docsUrl = process.env.DOCS_URL || '#';
   
   res.status(200).json({ 
-    logo_url: logoUrl 
+    logo_url: logoUrl,
+    docs_url: docsUrl
   });
 }
